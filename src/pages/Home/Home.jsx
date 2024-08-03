f5466d77d2e99776c03ba0bae21805160909533a
ustymenko-d <ustymenko.denys@gmail.com>
2024-08-03 21:59:18 +0300

    useResponsive hook

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,40 @@
-import { useEffect, useState } from 'react';
-import { Context } from '../../store/Context';
-import { InfoBlock } from './components/InfoBlock/InfoBlock';
-import { Actions } from './components/Actions/Actions';
-import './Home.css';
+import { useEffect, useState } from 'react'
+import { Context } from '../../store/Context'
+import { useResponsiveJSX } from '../../hooks/useResponsiveJSX'
+import { InfoBlock } from './components/InfoBlock/InfoBlock'
+import { Actions } from './components/Actions/Actions'
+import './Home.css'
 
 const Home = () => {
-	const [count, setCount] = useState(0);
-	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	const [count, setCount] = useState(0)
+	const [isDarkTheme, setIsDarkTheme] = useState(false)
+	const breakpoints = useResponsiveJSX([768, 1024])
 
 	useEffect(() => {
-		const mq = window.matchMedia('(prefers-color-scheme: dark)');
+		const mq = window.matchMedia('(prefers-color-scheme: dark)')
 
 		if (mq.matches) {
-			setIsDarkTheme(true);
+			setIsDarkTheme(true)
 		}
 
-		mq.addEventListener('change', (evt) => setIsDarkTheme(evt.matches));
-	}, []);
+		mq.addEventListener('change', (evt) => setIsDarkTheme(evt.matches))
+	}, [])
 
 	return (
 		<Context.Provider value={{ count, setCount, isDarkTheme, setIsDarkTheme }}>
 			<div className={isDarkTheme ? 'home home_dark' : 'home home_light'}>
 				<h1>React state management</h1>
 
+				{breakpoints === 0 && <h2>Small screen</h2>}
+				{breakpoints === 1 && <h2>Medium screen</h2>}
+				{breakpoints === 2 && <h2>Large screen</h2>}
+
 				<InfoBlock />
 
 				<Actions />
 			</div>
 		</Context.Provider>
-	);
-};
+	)
+}
 
-export default Home;
+export default Home
