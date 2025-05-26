b810bd3d038fb759bcbecaa4e87ba13de87f8b14
ustymenko-d <ustymenko.denys@gmail.com>
2025-05-26 19:47:53 +0300

    Refactor main.tsx to streamline imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { persistor, store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import Home from './pages/Home/Home'
 import './index.css'
-import { PersistGate } from 'redux-persist/integration/react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<StrictMode>
 		<Provider store={store}>
 			<PersistGate
 				loading={null}
@@ -15,5 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 				<Home />
 			</PersistGate>
 		</Provider>
-	</React.StrictMode>
+	</StrictMode>
 )
